81872fd09cdd970b2099568719fc00859bdd9886
DavidMeu <mdavid10@gmail.com>
2018-06-21 14:26:26 +0300

    D

--- src/Testing/RenderTest.java
+++ src/Testing/RenderTest.java
@@ -8,6 +8,8 @@ import Renderer.Render;
 import Scenes.Scene;
 import org.junit.Test;
 
+import java.awt.*;
+
 
 public class RenderTest {
 
@@ -17,31 +19,31 @@ public class RenderTest {
     public void basicRendering(){
 
         Scene scene = new Scene();
-
-        scene.addGeometry(new Sphere(50, new Point3D(0.0, 0.0, -50)));
-
-        Triangle triangle = new Triangle(new Point3D( 100, 0, -49),
-                new Point3D(  0, 100, -49),
-                new Point3D( 100, 100, -49));
-
-        Triangle triangle2 = new Triangle(new Point3D( 100, 0, -49),
-                new Point3D(  0, -100, -49),
-                new Point3D( 100,-100, -49));
-
-        Triangle triangle3 = new Triangle(new Point3D(-100, 0, -49),
-                new Point3D(  0, 100, -49),
-                new Point3D(-100, 100, -49));
-
-        Triangle triangle4 = new Triangle(new Point3D(-100, 0, -49),
-                new Point3D(  0,  -100, -49),
-                new Point3D(-100, -100, -49));
-
+scene.setScreenDistance(150);
+        Sphere s=new Sphere(100, new Point3D(0.0, 0.0, -600));
+s.setEmission(new Color(0,0,225));
+        Triangle triangle = new Triangle(new Point3D( 100, 0, -150),
+                new Point3D(  0, 100, -150),
+                new Point3D( 100, 100, -150));
+triangle.setEmission(new Color(0,200,0));
+        Triangle triangle2 = new Triangle(new Point3D( 100, 0, -150),
+                new Point3D(  0, -100, -150),
+                new Point3D( 100,-100, -150));
+triangle2.setEmission(new Color(200,0,0));
+        Triangle triangle3 = new Triangle(new Point3D(-100, 0, -150),
+                new Point3D(  0, 100, -150),
+                new Point3D(-100, 100, -150));
+triangle3.setEmission(new Color(193,197,17));
+        Triangle triangle4 = new Triangle(new Point3D(-100, 0, -150),
+                new Point3D(  0,  -100, -150),
+                new Point3D(-100, -100, -150));
+triangle4.setEmission(new Color(0,230,2));
         scene.addGeometry(triangle);
         scene.addGeometry(triangle2);
         scene.addGeometry(triangle3);
         scene.addGeometry(triangle4);
-
-        ImageWriter imageWriter = new ImageWriter(IMAGES_TEST_DIR + "Render test 2", 500, 500, 500, 500);
+scene.addGeometry(s);
+        ImageWriter imageWriter = new ImageWriter( "Render test 2", 500, 500, 500, 500);
 
         Render render = new Render(imageWriter, scene);
 
